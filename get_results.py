0beab788aa6a24b0f97ceccb9c98cb29021cac4f
Gerit Wagner <gerit1989@googlemail.com>
2014-07-13 12:48:32 +0200

    Code Cleaning

--- get_results.py
+++ get_results.py
@@ -5,23 +5,20 @@ import httplib2
 from bs4 import BeautifulSoup, SoupStrainer
 import win32clipboard
 import xlsxwriter
-
-
 http = httplib2.Http()
 
-outfile = open('output.txt', 'w', encoding='utf-8')
+result_url = "http://blv-sport.de/service/msonline/files/20145720-e.htm"
+
+print("Retrieving URL: "+result_url)
+
+outfile = open('output.txt', 'w')
 workbook = xlsxwriter.Workbook('result_export.xlsx')
 worksheet = workbook.add_worksheet()
 
-
-status, response = http.request('http://blv-sport.de/service/msonline/files/20145720-e.htm', "GET")
+status, response = http.request(result_url, "GET")
 response = response.decode('iso-8859-1')
 
 soup = BeautifulSoup(response)
-
-soup.style.decompose()
-soup.head.decompose()
-
 
 Meisterschaft = soup.select(".KopfZ11")[0].get_text()
 Datum_Ort = soup.select(".KopfZ12")[0].get_text()
@@ -30,34 +27,36 @@ position_in = Datum_Ort.find("in")
 Datum = Datum_Ort[3:position_in]
 Ort = Datum_Ort[position_in+3:]
 
+print("Extracting: ")
 print("Meisterschaft: " + Meisterschaft)
 print("Datum: " + Datum)
 print("Ort: " + Ort)
 
+soup.style.decompose()
+soup.head.decompose()
 soup.select(".KopfZ1")[0].parent.parent.decompose()
 soup.select(".KopfZ21")[0].parent.parent.decompose()
 soup.select(".KopfZ2")[0].parent.parent.decompose()
 
-
-heading_veranstaltungs_bericht = soup.find("a", {"name":"VERANSTALTUNGS-BERICHT"}).parent.parent.parent.parent
-
 data = soup.prettify()
-
 pos_bericht = data.find('name="VERANSTALTUNGS-BERICHT"')
-
-print(pos_bericht)
-
 data = data[:pos_bericht]
-
 soup = BeautifulSoup(data)
+
 # remove all <br/>
 brs = soup.find_all("br")
 for tag in brs:
-		soup.br.unwrap()
+	soup.br.unwrap()
+
+		
 
 worksheet.write(0, 0, Meisterschaft)
 worksheet.write(0, 1, Datum)
 worksheet.write(0, 2, Ort)
+
+worksheet.set_column('A:A', 10)
+worksheet.set_column('B:B', 30)
+worksheet.set_column('E:E', 30)
 
 row_excel=1
 tables = soup.find_all("table")
@@ -76,10 +75,9 @@ for table in tables:
 			worksheet.write(row_excel, column_excel, text)
 			column_excel = column_excel + 1
 
-
-
-
 workbook.close()	
 
 outfile.write(soup.prettify())	
 outfile.close()
+
+input("Export successful. Press Enter to close.")
